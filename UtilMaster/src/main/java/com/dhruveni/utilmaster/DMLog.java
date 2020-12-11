db57b41ed463f6f5e25bb8faf267dde7400efdfb
kishan Rathod <31632976+Kishan7up@users.noreply.github.com>
2020-12-11 15:54:55 +0530

    Intial Commit

---
+++ UtilMaster/src/main/java/com/dhruveni/utilmaster/DMLog.java
@@ -0,0 +1,12 @@
+package com.dhruveni.utilmaster;
+
+import android.util.Log;
+
+public class DMLog {
+
+    public static String TAG = "dhruveni-Log";
+    public static void DmLogMsg(String msg)
+    {
+        Log.d(TAG,msg);
+    }
+}
