9d5e14238630837d6c61e29a742a9591114fcf94
baps <rathod.kishan7up@gmail.com>
2021-01-16 18:01:12 +0530

    New Instagram Activitiy For Image Filter

---
+++ UtilMaster/src/main/java/com/dhruveni/utilmaster/InstaGramFilter.java
@@ -0,0 +1,14 @@
+package com.dhruveni.utilmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InstaGramFilter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insta_gram_filter);
+    }
+}
