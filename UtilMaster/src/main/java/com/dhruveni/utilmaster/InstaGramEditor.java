32def72d1aef895e965e0692af22d198ced31584
baps <rathod.kishan7up@gmail.com>
2021-01-16 17:08:37 +0530

    InstaGram Library Toast

--- UtilMaster/src/main/java/com/dhruveni/utilmaster/InstaGramEditor.java
+++ UtilMaster/src/main/java/com/dhruveni/utilmaster/InstaGramEditor.java
@@ -3,6 +3,7 @@ package com.dhruveni.utilmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class InstaGramEditor extends AppCompatActivity {
 
@@ -11,5 +12,6 @@ public class InstaGramEditor extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insta_gram_editor);
 
+        Toast.makeText(this, "Helllooooo", Toast.LENGTH_SHORT).show();
     }
 }
